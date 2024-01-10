ef3f61e2718f8f6e183bc96317e57d34a6c4b448
202100270AC <101176327+202100270AC@users.noreply.github.com>
2024-01-10 22:30:12 +0000

    ...

--- Projeto-Código/Municip.io/Municip.io.Server/obj/Debug/net8.0/Municip.io.Server.AssemblyInfo.cs
+++ Projeto-Código/Municip.io/Municip.io.Server/obj/Debug/net8.0/Municip.io.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+766b9c571ea9b4e0226b207118fd2040fa4ea870")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272f9fbc88b36a52e4974d209cc2db3de19e3289")]
 [assembly: System.Reflection.AssemblyProductAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
