2b8f37cde58418c6c43e07e381342e85381f81a2
pedromouraips <202100110@estudantes.ips.pt>
2024-01-10 22:05:26 +0000

    Ligação com GeoAPI - Controlador e primeiro contacto
    Criação do controlador e primeira comunicação com GeoAPI
    
    Criei um método que possibilita, através de string, a aquisição dos dados de determinado município.
    Será utilizado aquando do registo da câmara municipal

---
+++ Projeto-Código/Municip.io/Municip.io.Server/Controllers/GeoAPIController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Municip.io.Server.Controllers
+{
+    [ApiController]
+    [Route("api/municipio")]
+    public class GeoAPIController : Controller
+    {
+      
+      
+            private readonly HttpClient _httpClient;
+
+            public GeoAPIController(IHttpClientFactory httpClientFactory)
+            {
+                _httpClient = httpClientFactory.CreateClient();
+                _httpClient.BaseAddress = new Uri("https://json.geoapi.pt/");
+            }
+
+            [HttpGet("{nomeMunicipio}")]
+            public async Task<IActionResult> ObterInformacoesMunicipio(string nomeMunicipio)
+            {
+                var response = await _httpClient.GetAsync($"municipio/{nomeMunicipio}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var informacoesMunicipio = JsonSerializer.Deserialize<InformacoesMunicipio>(content);
+                    return Ok(informacoesMunicipio);
+                }
+
+                return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+            }
+        }
+
+    }
+
---
+++ Projeto-Código/Municip.io/Municip.io.Server/Models/InformacoesMunicipio.cs
@@ -0,0 +1,21 @@
+﻿public class InformacoesMunicipio
+{
+    public string codigo { get; set; }
+    public string nif { get; set; }
+    public string rua { get; set; }
+    public string localidade { get; set; }
+    public string codigopostal { get; set; }
+    public string descrpostal { get; set; }
+    public string email { get; set; }
+    public string telefone { get; set; }
+    public string fax { get; set; }
+    public string sitio { get; set; }
+    public string areaha { get; set; }
+    public string populacao { get; set; }
+    public string eleitores { get; set; }
+    public string codigoine { get; set; }
+    public string nome { get; set; }
+    public string distrito { get; set; }
+
+}
+
