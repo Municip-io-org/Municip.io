7916a5365d2fc605172632088e7fefa431f8b5c0
pedromouraips <100920362+pedromouraips@users.noreply.github.com>
2024-01-17 11:15:01 +0000

    Select para admin municipal.
    Aplicação da mesma lógica na nova página

--- Projeto-Código/Municip.io/municip.io.client/src/app/signUp/sign-up-municipal-administrator-account/sign-up-municipal-administrator-account.component.ts
+++ Projeto-Código/Municip.io/municip.io.client/src/app/signUp/sign-up-municipal-administrator-account/sign-up-municipal-administrator-account.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { Citizen, CitizenAuthService } from '../../services/citizen-auth.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Municipalities } from '../../municipalities.enum';
 
 @Component({
   selector: 'app-sign-up-municipal-administrator-account',
   templateUrl: './sign-up-municipal-administrator-account.component.html',
   styleUrl: './sign-up-municipal-administrator-account.component.css'
 })
+
+
 export class SignUpMunicipalAdministratorAccountComponent {
   citizen: Citizen = {
     firstName: '',
@@ -23,6 +26,11 @@ export class SignUpMunicipalAdministratorAccountComponent {
     birthDate: new Date()
   };
 
+  municipalities = Municipalities;
+
+  getValues() {
+    return Object.values(this.municipalities)
+  }
   constructor(private citizenAuthService: CitizenAuthService, private router: Router) { }
 
   signUpCitizenForm = new FormGroup({
