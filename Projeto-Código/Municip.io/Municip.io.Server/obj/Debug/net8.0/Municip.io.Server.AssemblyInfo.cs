b402ab805674f99053a450a69ae5653c3cd7f328
202100270AC <101176327+202100270AC@users.noreply.github.com>
2024-01-17 15:48:51 +0000

    LAYOUT - Registo Municipe completo

--- Projeto-Código/Municip.io/Municip.io.Server/obj/Debug/net8.0/Municip.io.Server.AssemblyInfo.cs
+++ Projeto-Código/Municip.io/Municip.io.Server/obj/Debug/net8.0/Municip.io.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd522f0ad5be2b6a31e51d22764dc1e53c53e6ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
