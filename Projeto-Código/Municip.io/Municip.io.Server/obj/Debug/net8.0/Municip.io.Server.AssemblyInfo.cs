6596fa37c7fd47c60a7114029cc2923ec12cbb88
pedromouraips <100920362+pedromouraips@users.noreply.github.com>
2024-01-22 14:58:53 +0000

    Loading Spinner - Concluído entre páginas
    Criação do interceptor, serviço e componente para loader!

--- Projeto-Código/Municip.io/Municip.io.Server/obj/Debug/net8.0/Municip.io.Server.AssemblyInfo.cs
+++ Projeto-Código/Municip.io/Municip.io.Server/obj/Debug/net8.0/Municip.io.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a679d6af1d620a3e65d12f2305de6d3f7b1dac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
