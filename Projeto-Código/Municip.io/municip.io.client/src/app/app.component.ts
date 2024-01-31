39e9e636d520ab37af3b7017380c57ad16d2cd59
RuiJesusIps <202100279@estudantes.ips.pt>
2024-01-31 11:48:35 +0000

    eliminar weatherforecast

--- Projeto-Código/Municip.io/municip.io.client/src/app/app.component.ts
+++ Projeto-Código/Municip.io/municip.io.client/src/app/app.component.ts
@@ -16,24 +16,14 @@ interface WeatherForecast {
   animations: [slideInAnimation],
 })
 export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
+
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getForecasts();
+
   }
 
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
 
   title = 'municip.io.client';
 }
