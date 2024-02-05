3100c0766ddf78422dd7dc6f09b499869c4267cf
RuiJesusIps <202100279@estudantes.ips.pt>
2024-02-05 12:54:41 +0000

    more

--- Projeto-Código/Municip.io/Municip.io.Server/obj/Debug/net8.0/Municip.io.Server.AssemblyInfo.cs
+++ Projeto-Código/Municip.io/Municip.io.Server/obj/Debug/net8.0/Municip.io.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5aa7ef4fb35b37fd9f6d3aa74f1e01b23bb232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
