273af10ba845359201f1a45b0a15933ff92bdc46
202100270AC <101176327+202100270AC@users.noreply.github.com>
2024-02-06 15:06:54 +0000

    card my mynicipe compleot

--- Projeto-Código/Municip.io/Municip.io.Server/obj/Debug/net8.0/Municip.io.Server.AssemblyInfo.cs
+++ Projeto-Código/Municip.io/Municip.io.Server/obj/Debug/net8.0/Municip.io.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d20091f391043da0997caec493895075eda2f0ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Municip.io.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
