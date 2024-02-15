e50b119534e6a5267dd11b8e34336087ce0e5ace
Carlos-Luz <202100311@estudantes.ips.pt>
2024-02-15 23:33:49 +0000

    newscard done

--- Projeto-Código/Municip.io/municip.io.client/src/app/news/news-card/news-card.component.ts
+++ Projeto-Código/Municip.io/municip.io.client/src/app/news/news-card/news-card.component.ts
@@ -6,9 +6,9 @@ import { Component, Input } from '@angular/core';
   styleUrl: './news-card.component.css'
 })
 export class NewsCardComponent {
-  @Input() title: string= "TITULO ";
+  @Input() title: string= "TITULO da noticia";
   @Input() subtitle: string = "subsubsubsubsubsusbubsubsubsusb";
-  @Input() image: string = "";
+  @Input() image: string = "/assets/images/admin/banner.jpg";
   @Input() date: string = "27/3/2012";
 
 }
