0b4cc403cd7a7254db516f5d7d335c6bc366d86f
pedromouraips <202100110@estudantes.ips.pt>
2024-02-15 15:41:57 +0000

    SessionStorage de login - getUserData através de sessão.
    Adaptação do método original, para que não seja necessário mudar código..
    Se os dados existirem em sessão, não faz a call ao servidor

--- Projeto-Código/Municip.io/municip.io.client/src/app/services/user-auth.service.ts
+++ Projeto-Código/Municip.io/municip.io.client/src/app/services/user-auth.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, catchError, map, of, switchMap } from 'rxjs';
-import { Citizen } from './citizen-auth.service';
-import { MunicipalAdministrator, Municipality } from './municipal-admin-auth.service';
+import { BehaviorSubject, Observable, Subject, of, throwError } from 'rxjs';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +14,20 @@ export class UserAuthService {
   public onStateChanged() {
     return this._authStateChanged.asObservable();
   }
-
-
   constructor(private http: HttpClient) { }
 
+  getUserDataFromStorage(): any {
+    console.log("foi buscar")
+    const userData = sessionStorage.getItem('userData');
+    return userData ? JSON.parse(userData) : null;
+  }
 
+  setUserDataToStorage(userData: any): void {
+    console.log("define em sessin")
+    sessionStorage.setItem('userData', JSON.stringify(userData));
+  }
 
-
-  login(login: Login, useCookies: boolean, useSessionCookies: boolean): Observable<boolean>   {
-
+  login(login: Login, useCookies: boolean, useSessionCookies: boolean): Observable<boolean> {
     let params = new HttpParams()
       .set('useCookies', useCookies.toString())
       .set('useSessionCookies', useSessionCookies.toString());
@@ -31,96 +35,89 @@ export class UserAuthService {
       params,
       observe: 'response',
       responseType: 'text'
-      }
-    )
-      .pipe<boolean>(map((res: HttpResponse<string>) => {
-        this._authStateChanged.next(res.ok);
+    }).pipe(
+      map((res: HttpResponse<string>) => {
+        this.setUserDataToStorage(login);
         return res.ok;
-      }));
-
-
+      })
+    );
+  }
+  getUserData(): Observable<any> {
+    const userData = this.getUserDataFromStorage();
+    if (userData) {
+      console.log("Aqui já tem os dados: ")
+      console.log(userData)
+      return of(userData);
+    } else {
+      console.log("aqui foi buscá-los")
+      return this.http.get<any>('/api/accounts/UserInfo').pipe(
+        tap(user => this.setUserDataToStorage(user)), 
+        catchError(error => {
+          console.error(error);
+          return throwError(error);
+        })
+      );
+    }
   }
 
-  getUserData() {
-    return this.http.get<any>('/api/accounts/UserInfo');
+  getInfoByEmail(email: string): Observable<any> {
+
+    return this.http.get<any>(`/api/accounts/InfoByEmail/?email=${email}`);
   }
 
-  getInfoByEmail(email: string) {
-    let params = new HttpParams().set('email', email);
-    return this.http.get<MunicipalAdministrator | Citizen>(`/api/accounts/InfoByEmail/?email=${email}`, {});
+  getUserRole(): Observable<any> {
+    return this.http.get<any>('/api/accounts/UserRole');
   }
 
-  getUserRole() {
-    return this.http.get<Role>('/api/accounts/UserRole');
-  }
-
-  getMunicipality() {
+  getMunicipality(): Observable<any> {
     return this.getUserData().pipe(
-      switchMap((userInfo) =>
+      switchMap(userInfo =>
         this.getInfoByEmail(userInfo.email).pipe(
-          map((user) => {
-            if (user.municipality) {
-              return user.municipality;
-            }
-            return null;
-          }),
-          catchError((_) => {
-            return of(null);
-          })
+          map(user => user.municipality || null),
+          catchError(_ => of(null))
         )
       )
     );
   }
 
-  getInfoMunicipality(nome: string) {
-    return this.http.get<Municipality>('/api/accounts/InfoMunicipality?name=' + nome);
+  getInfoMunicipality(nome: string): Observable<any> {
+    return this.http.get<any>(`/api/accounts/InfoMunicipality?name=${nome}`);
   }
 
+  updateUser(user: any, image: File): Observable<any> {
+    const headers = new HttpHeaders({ 'authorization': 'Client-ID a9e7323ad868dd2' });
+    const imgurl = "https://api.imgur.com/3/image";
+    const formData = new FormData();
+    formData.append('image', image);
+    return this.http.post(imgurl, formData, { headers }).pipe(
+      switchMap((response: any) => {
+        user.photo = response['data']['link'];
+        return this.http.put('/api/accounts/UpdateUserInfo', user);
+      })
+    );
+  }
 
-
-
-  signOut() {
+  signOut(): Observable<boolean> {
     return this.http.post('/api/accounts/logout', {}, {
       withCredentials: true,
       observe: 'response',
       responseType: 'text'
-    }).pipe<boolean>(map((res: HttpResponse<string>) => {
-      if (res.ok) {
-        this._authStateChanged.next(false);
-      }
-      return res.ok;
-    }));
+    }).pipe(
+      map((res: HttpResponse<string>) => {
+        if (res.ok) {
+          this.setUserDataToStorage(null); // Limpa os dados do usuário ao sair
+          return true;
+        }
+        return false;
+      })
+    );
   }
-
-
-
 
   isSignedIn(): Observable<boolean> {
     return this.getUserData().pipe(
-      map((userInfo) => {
-        const valid = !!(userInfo && userInfo.email && userInfo.email.length > 0);
-        return valid;
-      }),
-      catchError((_) => {
-        return of(false);
-      }));
-  }
-
-  updateUser(user: Citizen,image:File) {
-    var headers = new HttpHeaders({ 'authorization': 'Client-ID a9e7323ad868dd2' });
-    let imgurl = "https://api.imgur.com/3/image";
-
-    //upload to imgur
-    const formData = new FormData();
-    formData.append('image', image);
-    return this.http.post(imgurl, formData, { headers })
-      .pipe(switchMap((response: any) => {
-        console.log(response);
-        user.photo = response['data']['link'];
-        console.log(user);
-        console.log(user.photo);
-        return this.http.put('/api/accounts/UpdateUserInfo', user);
-      }));
+      map(userInfo => !!userInfo && !!userInfo.email),
+      catchError(_ => of(false))
+    );
   }
 }
 
@@ -131,16 +128,10 @@ export interface Login {
   email: string,
   twoFactorCode: string,
   twoFactorRecoveryCode: string
-
 }
-
 
 export enum Roles {
   Admin = "Admin",
-  Municipal= "Municipal",
-  Citizen= "Citizen"
-}
-
-export interface Role {
-role: Roles
+  Municipal = "Municipal",
+  Citizen = "Citizen"
 }
