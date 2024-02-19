bce2ba7e7a06fa3d4a1b930c956e02dceed91071
pedromouraips <202100110@estudantes.ips.pt>
2024-02-19 11:30:57 +0000

    Landing header fix
    Header ainda tinha terminar sessão... mas não existirá sessão na landing

--- Projeto-Código/Municip.io/municip.io.client/src/app/layout/header/header.component.ts
+++ Projeto-Código/Municip.io/municip.io.client/src/app/layout/header/header.component.ts
@@ -8,23 +8,12 @@ import { UserAuthService } from '../../services/user-auth.service';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  public isSignedIn: boolean = false;
 
   constructor(private auth: UserAuthService, private router: Router) { }
 
   ngOnInit() {
-    this.auth.onStateChanged().forEach((state: any) => {
-      this.auth.isSignedIn().forEach((signedIn: boolean) => this.isSignedIn = signedIn);
-    });
+
   }
 
-  signOut() {
-    if (this.isSignedIn) {
-      this.auth.signOut().forEach(response => {
-        if (response) {
-          this.router.navigateByUrl('');
-        }
-      });
-    }
+ 
   }
-}
