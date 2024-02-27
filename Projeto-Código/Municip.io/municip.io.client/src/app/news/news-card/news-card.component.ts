a5da17898412c762dbb5c52b071ba42da99b7478
Carlos-Luz <202100311@estudantes.ips.pt>
2024-02-27 19:46:02 +0000

    bug fixes e ajustes nas paginas para flex.

--- Projeto-Código/Municip.io/municip.io.client/src/app/news/news-card/news-card.component.ts
+++ Projeto-Código/Municip.io/municip.io.client/src/app/news/news-card/news-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { News, NewsService } from '../../services/news/news.service';
+import { Roles, UserAuthService } from '../../services/user-auth.service';
 
 @Component({
   selector: 'app-news-card',
@@ -14,7 +15,24 @@ export class NewsCardComponent {
   @Input() id: string ="";
   @Output() deleteid = new EventEmitter<string>();;
 
-  constructor(private newsService: NewsService) { }
+  constructor(private newsService: NewsService, private userAuthService: UserAuthService) { }
+
+  role : string = "";
+
+  ngOnInit() {
+    this.userAuthService.getUserRole().subscribe(
+      res => {
+        if (res.role == Roles.Citizen) {
+          this.role = res.role;
+
+        }
+      },
+      error => {
+        console.error(error);
+      }
+    );
+
+  }
 
   deleteCurrent(news: any) {
     this.deleteid.emit(news.id);
