6567b9c37ed0991e8e50537718d7b40112fe3948
RuiJesusIps <202100279@estudantes.ips.pt>
2024-03-08 09:10:01 +0000

    enviar email no registo

---
+++ Projeto-Código/Municip.io/Municip.io.Server/Models/AccountUserEmail.cs
@@ -0,0 +1,25 @@
+﻿namespace Municip.io.Server.Models
+{
+    public enum AccountUserEmail
+    {
+        REGISTER
+    }
+
+
+
+    public static class AccountUserEmailExtensions
+    {
+        public static string toString(this AccountUserEmail me)
+        {
+            switch (me)
+            {
+               
+                case AccountUserEmail.REGISTER:
+                    return "Acabou de se <span style='font-weight: bold;'>registar na nossa plataforma com sucesso</span>. Aguarde enquanto a nossa equipa avalia o seu pedido.";
+                default:
+                    return "Desconhecido";
+            }
+        }
+    }
+
+}
