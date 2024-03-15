23085a5480c5868911d4cf4807b0fdd86ffe0802
RuiJesusIps <202100279@estudantes.ips.pt>
2024-03-15 20:53:13 +0000

    realização de pagamentos atraves de api
    Realiza pagamentos e escuta eventos de bem sucedido

---
+++ Projeto-Código/Municip.io/Municip.io.Server/Controllers/StripePaymentController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Municip.io.Server.Models;
+using Stripe;
+using Stripe.Checkout;
+using System.Text.Json;
+
+namespace Municip.io.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StripePaymentController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Create([FromBody] string email)
+        {
+            StripeConfiguration.ApiKey = "sk_test_51OuXzxP2EBv6AKDvatVzGyyqukIMntPmnsT67WMRWkzgbNluAoxnlH4okVTPXKRtriLWUiWJS2EuWNxGiifuV79700Vbj1wZUi";
+            var domain = "http://localhost:4242";
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+
+                        Price =  "price_1OuZ0eP2EBv6AKDvsLRT6YlD",
+                        Quantity = 1,
+                    },
+                },
+                Mode = "payment",
+                SuccessUrl = domain + "/success.html",
+                CancelUrl = domain + "/cancel.html",
+                
+                //associate the session with the user
+                CustomerEmail = email,
+
+            };
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            var json = JsonSerializer.Serialize(session.Url);
+
+            return Content(json, "application/json");
+        }
+
+
+
+        //check if the payment was successful
+        [HttpPost("verify")]
+        public ActionResult Verify(string pi)
+        {
+            StripeConfiguration.ApiKey = "sk_test_51OuXzxP2EBv6AKDvatVzGyyqukIMntPmnsT67WMRWkzgbNluAoxnlH4okVTPXKRtriLWUiWJS2EuWNxGiifuV79700Vbj1wZUi";
+            var service = new PaymentIntentService();
+            var paymentIntent = service.Get(pi);
+
+            return Content(JsonSerializer.Serialize(paymentIntent.Status), "application/json");
+
+        }
+
+
+       
+    }
+
+
+}
---
+++ Projeto-Código/Municip.io/Municip.io.Server/Controllers/StripeWebHookController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using System;
+using System.IO;
+
+using Stripe.Checkout;
+namespace Municip.io.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StripeWebHookController : ControllerBase
+    {
+        const string endpointSecret = "whsec_604da9682838b3059bdcf61fc84de1b923f84f15e3d2841fe1c89c609ce66448";
+
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            try
+            {
+                var stripeEvent = EventUtility.ConstructEvent(
+                  json,
+                  Request.Headers["Stripe-Signature"],
+                  secret: endpointSecret
+                );
+
+                // Handle the checkout.session.completed event
+                if (stripeEvent.Type == Events.CheckoutSessionCompleted)
+                {
+                    var session = stripeEvent.Data.Object as Stripe.Checkout.Session;
+                    var options = new SessionGetOptions();
+                    options.AddExpand("line_items");
+
+                    var service = new SessionService();
+                    // Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+                    Session sessionWithLineItems = service.Get(session.Id, options);
+                    StripeList<LineItem> lineItems = sessionWithLineItems.LineItems;
+
+                    // Fulfill the purchase...
+                    this.FulfillOrder(lineItems);
+                }
+
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                return BadRequest();
+            }
+        }
+
+        private void FulfillOrder(StripeList<LineItem> lineItems)
+        {
+
+            Console.WriteLine(lineItems);
+        }
+    }
+}
+
