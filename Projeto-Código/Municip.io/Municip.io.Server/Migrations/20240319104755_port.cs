41ffde13edac4988127b51bb3eaf1aed77cd66bc
pedromouraips <100920362+pedromouraips@users.noreply.github.com>
2024-03-19 11:31:12 +0000

    Correction navigate and Docs compodoc

---
+++ Projeto-Código/Municip.io/Municip.io.Server/Migrations/20240319104755_port.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Municip.io.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class port : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
