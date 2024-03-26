214af94032c3316fd2d53bc61611f44e7f530193
202100270AC <101176327+202100270AC@users.noreply.github.com>
2024-03-26 11:32:51 +0000

    GetBookInfoAPI

--- Projeto-Código/Municip.io/Municip.io.Server/Controllers/BookController.cs
+++ Projeto-Código/Municip.io/Municip.io.Server/Controllers/BookController.cs
@@ -7,5 +7,30 @@ namespace Municip.io.Server.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
+        private readonly string _apiKey = "AIzaSyBG6nzqVZ5MjockppHITseDjF72nEKnHeQ";
+
+        private readonly HttpClient _httpClient;
+
+        public BookController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [HttpGet("GetBookInfoAPI")]
+        public async Task<IActionResult> GetBookInfoAPI(string isbn)
+        {
+            var response = await _httpClient.GetAsync($"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+                return Content(content, "application/json"); // Retorna o JSON com o tipo de conteúdo definido
+            }
+
+            return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+        }
     }
 }
+
+
