baba56dea0a95681eb4ecc79f8b44bd67f2b0adb
Carlos-Luz <100920507+Carlos-Luz@users.noreply.github.com>
2024-04-08 20:41:14 +0100

    filtro do ano working bem(por ano )

--- Projeto-Código/Municip.io/municip.io.client/src/app/library/library-list/library-list.component.ts
+++ Projeto-Código/Municip.io/municip.io.client/src/app/library/library-list/library-list.component.ts
@@ -14,12 +14,16 @@ export class LibraryListComponent {
   books: Book[] = [];
   nameSearch: string = '';
     user: any;
-    municipality: any;
+  municipality: any;
+    filteredBookList: Book[] = [];
   filterWindow: boolean = true;
   isbnSearch: string = '';
   authorSearch: string = '';
-  dateSearch: string = '';
+  dateSearch: string ='';
   genreSearch: string = '';
+  options: string[] = [];
+  dateOptions: string[] = [];
+  showDropdown: boolean = false;
 
   constructor(private userAuthService:UserAuthService, private libraryService: LibraryService,private router :Router) { }
 
@@ -37,18 +41,47 @@ export class LibraryListComponent {
             
             this.libraryService.getBooks(this.municipality).subscribe((data: Book[]) => {
               this.books = data;
-              this.books = this.books.filter(b => b.status != BookStatus.Unavailable)
+              this.books = this.books.filter(b => b.status != BookStatus.Unavailable);
+              
+              this.loadDateOptions();
             });
-            
+
+
           }
         )
       });
+    
   }
 
   get filteredBooks() {
-    if (this.nameSearch == '') return this.books;
-    return this.books.filter(e => e.title.toLowerCase().includes(this.nameSearch.toLowerCase()));
+    let filteredList = this.books;
+
+    if (this.nameSearch !== '') {
+      filteredList = filteredList.filter(book => book.title.toLowerCase().includes(this.nameSearch.toLowerCase()));
+    }
+
+    if (this.isbnSearch !== '') {
+      filteredList = filteredList.filter(book => book.isbn?.toLowerCase().includes(this.isbnSearch.toLowerCase()));
+    }
+
+    if (this.authorSearch !== '') {
+      filteredList = filteredList.filter(book => this.filterArray(book.author, this.authorSearch.toLowerCase()));
+    }
+
+    if (this.genreSearch !== '') {
+      filteredList = filteredList.filter(book => this.filterArray(book.genre, this.genreSearch.toLowerCase()));
+    }
+    if (this.dateSearch !== '') {
+      filteredList = filteredList.filter(book => book.publicationDate.toString().split("-")[0] === this.dateSearch);
+    }
+
+    return filteredList;
   }
+
+  filterArray(array: string[], searchTerm: string): boolean {
+    return array.some(item => item.toLowerCase().includes(searchTerm));
+  }
+
 
   editBook(book: any) {
     this.router.navigateByUrl('/library/edit/' + book.book.id);
@@ -59,7 +92,7 @@ export class LibraryListComponent {
     this.libraryService.removeBook(book.book.id).subscribe(
       (res) => {
         console.log("Livro apagado com sucesso", res);
-        this.books = this.books.filter(e => e.isbn !== book.isbn);
+       this.loadBooks();
       },
       (error) => {
         console.log(error);
@@ -67,7 +100,24 @@ export class LibraryListComponent {
     );
   }
 
+  loadBooks() {
+    this.libraryService.getBooks(this.municipality).subscribe((data: Book[]) => {
+      this.books = data;
+      this.books = this.books.filter(b => b.status != BookStatus.Unavailable)
+    });
+  }
+  
   OpenFilterWindow() {
     this.filterWindow = !this.filterWindow;
   }
+
+  loadDateOptions() {
+    
+      this.dateOptions =this.books.map(book => {
+        return book.publicationDate.toString().split("-")[0];
+      });
+    
+    console.log(this.dateOptions);
+  }
+
 }
